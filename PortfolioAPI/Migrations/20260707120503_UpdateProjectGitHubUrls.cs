1f1313cc3453d9360104ea0b1fdaec4c4c988bb1
7Oguzhan7 <oguzhanince604@example.com>
2026-07-07 15:06:10 +0300

    Update project GitHub URLs in database and migrations

---
+++ PortfolioAPI/Migrations/20260707120503_UpdateProjectGitHubUrls.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortfolioAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProjectGitHubUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "GitHubUrl",
+                value: "https://github.com/7Oguzhan7/Akilli-kutuphane-ve-rezervasyon-sistemi");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "GitHubUrl",
+                value: "https://github.com/7Oguzhan7/Kisisel-Portfolyo-Site");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "GitHubUrl",
+                value: "https://github.com/7Oguzhan7/grup-proje");
+
+            migrationBuilder.UpdateData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "GitHubUrl",
+                value: "");
+        }
+    }
+}
